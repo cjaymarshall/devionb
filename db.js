3e2aeea9373296d2c3ba0564c93f070dbdce3a31
C Jay Marshall <cjaymarshall1@hotmail.com>
2017-04-06 12:52:43 -0600

    add postgres

--- db.js
+++ db.js
@@ -1,8 +1,17 @@
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-	'dialect': 'sqlite',
-	'storage': 'data/iondb.sqlite'
-});
+var env = process.env.NODE_ENV || 'development';
+var sequelize;
+
+if(env === 'production') {
+	sequelize = new Sequelize(process.env.DATABASE_URL, {
+		dialect: 'postgres'
+	});
+} else {
+	var sequelize = new Sequelize(undefined, undefined, undefined, {
+		'dialect': 'sqlite',
+		'storage': 'data/iondb.sqlite'
+	});
+}
 
 var db = {};
 
